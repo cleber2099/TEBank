b7b163ecd89a4d027e4aa23440b5028d41e158d4
cleber2099 <primermito@gmail.com>
2022-04-25 07:01:21 -0300

    correção de big e testes implementados

--- src/main/java/ContaCorrente.java
+++ src/main/java/ContaCorrente.java
@@ -1,14 +1,20 @@
 public class ContaCorrente {
     private double saldo;
 
-    public void deposita(double valor) {
+    public void depositar(double valor) {
         this.saldo += valor;
     }
     public void sacar(double valor) {
-        this.saldo -= valor;
+        if(valor <= this.saldo) {
+    this.saldo -= valor;
+        }
+        else {
+            System.out.println("Saldo insuficiente");
+        }
     }
+
     public double getSaldo() {
         return this.saldo;
     }
-    
+
 }
